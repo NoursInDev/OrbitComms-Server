d8b1b680ec44e1709312966c8b4517207036a03f
NoursInDev <nours44.mc@gmail.com>
2025-02-19 17:03:01 +0100

    Code - update test base

---
+++ tests/player.test.js
@@ -0,0 +1,14 @@
+/*
+Test DATAs
+player1 :
+    globalPermissions 1
+    channels "channel1" : 2 "channels2" : 0 "channels3" : 1
+
+player2 :
+    globalPermissions 0
+    channels "channel1" : 2 "channels2" : 1 "channels3" : 2
+ */
+
+
+import {Player} from "../players/playermanager";
+
