679fddbe01de8b9a2b412e48046d036985905b62
NoursInDev <nours44.mc@gmail.com>
2025-02-19 16:20:39 +0100

    Code - audio byte distribution logic

--- audio/audioprocessor.js
+++ audio/audioprocessor.js
@@ -1,4 +1,4 @@
-import { Server } from "../server";
+import {Server} from "../server";
 
 export class AudioProcessor {
 
@@ -13,6 +13,24 @@ export class AudioProcessor {
     processAudioData(username, data) {
         if (!data || typeof data !== 'object' || !data.chan || !data.bytes) {
             throw new Error("Invalid data structure");
+        }
+
+        const player = this.server.players.getPlayer(username);
+        if (!player || !this.server.players.getPlayerChannels(username).includes(data.chan)) {
+            throw new Error("Player is not connected to the specified channel");
+        }
+
+        // send audio data to all players in the channel
+        if (data.chan === 'voip') {
+            //todo
+            console.error('TODO: Implement audio processing for voip channel');
+        } else {
+            const players = this.server.players.getChannelPLayerList(data.chan);
+            players.forEach(p => {
+                if (p.username !== username) {
+                    this.server.sendAudioData(p.username, data);
+                }
+            });
         }
     }
 }
