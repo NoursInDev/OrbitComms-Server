c42d217725f04335c955f927792cc41d5078678e
NoursInDev <nours44.mc@gmail.com>
2025-03-14 18:45:58 +0100

    Code - basic logger v1

---
+++ logger/logger.js
@@ -0,0 +1,13 @@
+const codename = "OrbitComms";
+
+const log = (msg) => {
+    console.log(`[${codename}] ${msg}`);
+}
+
+const warn = (msg) => {
+    console.warn(`[${codename}] ${msg}`);
+}
+
+const error = (msg) => {
+    console.error(`[${codename}] ${msg}`);
+}
